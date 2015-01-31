4e0e73ecf32d5362f00301567d23c0b9aa93d5a1
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2015-01-31 11:29:56 +0100

    Bake in use-case of multiple chords

--- randomchange.py
+++ randomchange.py
@@ -5,6 +5,7 @@ import cPickle as pickle
 import os.path
 import time
 import datetime
+import sys
 
 # Configuration
 chords = ["E", "A", "D", "Emin", "Amin", "Dmin"] # The chords from which combinations may be formed
@@ -17,73 +18,89 @@ playtimeSteps = 10 # Steps in which the playtime counter gets updated. Must be a
 
 numberOfChords = len(chords)
 
+def random_chord():
 # Load scores or initialize new score table, if none is found
-try:
-	with open(scorePath) as f: score = pickle.load(f)
-except IOError as e:
-	score = dict()
+	try:
+		with open(scorePath) as f: score = pickle.load(f)
+	except IOError as e:
+		score = dict()
 
-for i in range(0, numberOfChords - 1):
-	for j in range(i + 1, numberOfChords):
-		if (i, j) not in score: score[i, j] = 1 # Use 1 as standard value, since otherwise 1 / score would lead to division by zero
+	for i in range(0, numberOfChords - 1):
+		for j in range(i + 1, numberOfChords):
+			if (i, j) not in score: score[i, j] = 1 # Use 1 as standard value, since otherwise 1 / score would lead to division by zero
 
 # Get one random number for each chord change, use the change with the highest random number
 # Since the expected value of the random number is 1/score, this gives us a higher probability for chords with lower scores
-currentMax = float("-inf")
-for i in range(0, numberOfChords - 1):
-	for j in range(i + 1, numberOfChords):
-		randomVal = random.gauss(1 / score[i, j], variance)
-		if randomVal > currentMax:
-			currentMax = randomVal
-			first = i
-			second = j
+	currentMax = float("-inf")
+	for i in range(0, numberOfChords - 1):
+		for j in range(i + 1, numberOfChords):
+			randomVal = random.gauss(1 / score[i, j], variance)
+			if randomVal > currentMax:
+				currentMax = randomVal
+				first = i
+				second = j
 
 # Display the chord and update the score for it
-print "Change: " + chords[first] + " <-> " + chords[second]
+	print "Change: " + chords[first] + " <-> " + chords[second]
 
-raw_input("Press a key to start the timer")
+	raw_input("Press a key to start the timer")
 
-for i in range(warmup, 0, -warmupSteps):
-	print 'Starting in', i, 'seconds...'
-	time.sleep(warmupSteps)
+	for i in range(warmup, 0, -warmupSteps):
+		print 'Starting in', i, 'seconds...'
+		time.sleep(warmupSteps)
 
-print "-----------------GO-----------------"
+	print "-----------------GO-----------------"
 
-for i in range(playtime, 0, -playtimeSteps):
-	print i, "seconds to go"
-	time.sleep(playtimeSteps)
+	for i in range(playtime, 0, -playtimeSteps):
+		print i, "seconds to go"
+		time.sleep(playtimeSteps)
 
-print "----------------STOP----------------"
 
-newScore = int(raw_input("What is your current score for this chord change? "))
+	print "----------------STOP----------------"
+
+	newScore = int(raw_input("What is your current score for this chord change? "))
 # Weigh the new score and the previous average the same. This way older scores become "forgotten"
-score[first, second] = (score[first, second] + newScore) / 2
+	score[first, second] = (score[first, second] + newScore) / 2
 
 # Display the score list
-print ""
-print "-----------------------------"
-print "Your current average scores:"
-for i in range(0, numberOfChords - 1):
-	for j in range(i + 1, numberOfChords):
-		print "\t"  + chords[i] + " <-> " + chords[j] + ": " + str(score[i,j])
+	print ""
+	print "-----------------------------"
+	print "Your current average scores:"
+	for i in range(0, numberOfChords - 1):
+		for j in range(i + 1, numberOfChords):
+			print "\t"  + chords[i] + " <-> " + chords[j] + ": " + str(score[i,j])
 
-now = datetime.datetime.now()
-with open("record.csv", 'a') as f:
-	f.write(chords[i])
-	f.write(",")
-	f.write(chords[j])
-	f.write(",")
-	f.write(str(newScore))
-	f.write(",")
-	f.write(str(now.date()))
-	f.write(",")
-	f.write(str(now.time().hour))
-	f.write(":")
-	f.write(str(now.time().minute))
-	f.write(":")
-	f.write(str(now.time().second))
-	f.write("\n")
+	now = datetime.datetime.now()
+	with open("record.csv", 'a') as f:
+		f.write(chords[i])
+		f.write(",")
+		f.write(chords[j])
+		f.write(",")
+		f.write(str(newScore))
+		f.write(",")
+		f.write(str(now.date()))
+		f.write(",")
+		f.write(str(now.time().hour))
+		f.write(":")
+		f.write(str(now.time().minute))
+		f.write(":")
+		f.write(str(now.time().second))
+		f.write("\n")
 
 # Save the new scores
-with open(scorePath, 'w') as f:
-	pickle.dump(score, f)
+	with open(scorePath, 'w') as f:
+		pickle.dump(score, f)
+
+def main():
+	if len(sys.argv) == 1:
+		random_chord()
+	elif len(sys.argv) == 2:
+		try:
+			for i in range(int(sys.argv[1])):
+				random_chord()
+		except ValueError:
+			print "Usage: " + sys.argv[0] + " [number-of-repetitions]"
+	else:
+		print "Usage: " + sys.argv[0] + " [number-of-repetitions]"
+
+main()
